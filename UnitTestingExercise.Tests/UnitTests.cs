52a8ce4b7333d4085ede11d64d19e3922b931689
Javian Phillips <javianph3@gmail.com>
2025-09-24 16:06:10 -0500

    Added more tests

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,7 +6,10 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData(2, 3, 5, 10)] //Add test data <-------
+        [InlineData(2, 3, 5, 10)]
+        [InlineData(20, 1, 37, 58)]
+        [InlineData(67, 114, 25, 206)]
+        [InlineData(9, 524, 748, 1281)]//Add test data <-------
         public void AddTest(int num1, int num2, int num3, int expected)
         {
             //Start Step 3 here:
@@ -26,7 +29,10 @@ namespace UnitTestingExercise.Tests
         }
 
         [Theory]
-        [InlineData(8, 6, 2)]//Add test data <-------
+        [InlineData(8, 6, 2)]
+        [InlineData(220,20, 200)]
+        [InlineData(357, 119, 238)]
+        [InlineData(36, 9, 27)]//Add test data <-------
         public void SubtractTest(int minuend, int subtrhend, int expected)
         {
             //Start Step 5 here:
@@ -43,7 +49,10 @@ namespace UnitTestingExercise.Tests
         }
 
         [Theory]
-        [InlineData(4, 14, 56)]//Add test data <-------
+        [InlineData(4, 14, 56)]
+        [InlineData(5, 20, 100)]
+        [InlineData(15, 3, 45)]
+        [InlineData(64,32,2048)]//Add test data <-------
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
@@ -60,7 +69,10 @@ namespace UnitTestingExercise.Tests
         }
 
         [Theory]
-        [InlineData(25, 5, 5)]//Add test data <-------
+        [InlineData(25, 5, 5)]
+        [InlineData(60, 4, 15)]
+        [InlineData(651, 21, 31)]
+        [InlineData(45, 9, 5)]//Add test data <-------
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange
